0f787ac049138a124c6eec8e68cd5eec86547167
Mononkero <a.roshiari@gmail.com>
2022-06-08 11:34:27 +0300

    Homework15 ver.2

--- Homework15/Homework15.py
+++ Homework15/Homework15.py
@@ -1,16 +1,31 @@
+'''''''''''''''''''''''''''''''''''''''''''''
 a = int(input("Input a: \n"))
 b = int(input("Input b: \n"))
 c = input("Input arithmetic operation: \n")
 def arithmetic(a, b, c):
     if c == "+":
-      return  (a + b)
+      return  a + b
     elif c =="-":
-        return (a - b)
+        return a - b
     elif c =="/":
-        return (a / b)
+        return a / b
     elif c == "*":
-        return (a * b)
+        return a * b
     else:
         return ("Unknown operation")
 print("Result:")
+print(arithmetic(a, b, c))
+'''''''''''''''''''''''''''''''''''''''''''''''
+
+a = int(input("Input a: \n"))
+b = int(input("Input b: \n"))
+c = input("Input arithmetic operation: \n")
+def arithmetic(a, b, c):
+    return {
+        '+': lambda: a+b,
+        '-': lambda: a-b,
+        '*': lambda: a*b,
+        '/': lambda: a/b,
+    }.get(c, lambda: 'Unknown operation')()
+
 print(arithmetic(a, b, c))
