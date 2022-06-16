10b1e815cbdf16c14fe5b6aa71c1a67427c4b9b8
Mononkero <a.roshiari@gmail.com>
2022-06-16 11:18:47 +0300

    Update 1.1

--- Practice/Practice.py
+++ Practice/Practice.py
@@ -1,16 +1,15 @@
+import argparse
 
 l = login_values = {
-    "first_player": "password1",
-    "user_ololo": "password2"
+    "player": "password1",
+    "ololo": "password2"
 }
 
 def check_password(username: str, password) -> bool:
-    if username in l and l[username] == password:
-        return True
-    else:
-        return False
+    return l.get(username) == password
 
-def login(func):
+
+def decor_login(func):
     def inner(username, password):
         if check_password(username, password):
             return func(username, password)
@@ -20,23 +19,46 @@ def login(func):
     return inner
 
 
-@login
-def decor_login(username: str, password: str) -> bool:
+@decor_login
+def login(username: str, password: str) -> bool:
     return True
+
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--username", help="valid username")
+    parser.add_argument("-p", "--password", help="valid password")
+    return parser.parse_args()
+args = parser()
 
 attempts = 3
 attempt = 0
 
 while attempt < attempts:
     attempt += 1
-    username = input("Input username: \n>> ")
-    password = input("Input password: \n>> ")
 
-    if decor_login(username, password):
+    username = ""
+    password = ""
+
+    if attempt == 1:
+        if args.username is not None:
+            username = args.username
+        else:
+            username = input("Input username: \n>> ")
+        if args.password is not None:
+            password = args.password
+        else:
+            password = input("Input password: \n>> ")
+    else:
+        username = input("Input username: \n>> ")
+        password = input("Input password: \n>> ")
+
+    if login(username, password):
         print("Logged in")
         break
     else:
         print("Attempts left", attempts - attempt)
 else:
     print("Attempts expired")
+
+
 
