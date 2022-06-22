436187fc1a7ae39ae14751a2a2eab023735d4772
Mononkero <a.roshiari@gmail.com>
2022-06-22 11:41:53 +0300

    update2.1

--- Practice/Practice.py
+++ Practice/Practice.py
@@ -1,4 +1,6 @@
 import argparse
+from datetime import datetime
+from datetime import timedelta
 
 l = login_values = {
     "player": "password1",
@@ -23,42 +25,72 @@ def decor_login(func):
 def login(username: str, password: str) -> bool:
     return True
 
+
 def parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-u", "--username", help="valid username")
     parser.add_argument("-p", "--password", help="valid password")
     return parser.parse_args()
-args = parser()
 
-attempts = 3
-attempt = 0
 
-while attempt < attempts:
-    attempt += 1
+block_duration_minutes = 5
 
-    username = ""
-    password = ""
 
-    if attempt == 1:
-        if args.username is not None:
-            username = args.username
+def login_execution() -> bool:
+    args = parser()
+
+    attempts = 3
+    attempt = 0
+
+    while attempt < attempts:
+        attempt += 1
+
+        username = ""
+        password = ""
+
+        if attempt == 1:
+            if args.username is not None:
+                username = args.username
+            else:
+                username = input("Input username: \n>> ")
+            if args.password is not None:
+                password = args.password
+            else:
+                password = input("Input password: \n>> ")
         else:
             username = input("Input username: \n>> ")
-        if args.password is not None:
-            password = args.password
-        else:
             password = input("Input password: \n>> ")
-    else:
-        username = input("Input username: \n>> ")
-        password = input("Input password: \n>> ")
 
-    if login(username, password):
-        print("Logged in")
-        break
+        if login(username, password):
+            print("Logged in")
+            return True
+        else:
+            print("Attempts left", attempts - attempt)
     else:
-        print("Attempts left", attempts - attempt)
-else:
-    print("Attempts expired")
+        print("Attempts expired. You've been blocked for " + str(block_duration_minutes) + " minutes")
+        return False
+
+
+def if_not_blocked(last_wrong_attempt_time) -> bool:
+    return last_wrong_attempt_time == None or datetime.now() >= (
+                last_wrong_attempt_time + timedelta(minutes=block_duration_minutes))
+
+
+def main():
+    last_wrong_attempt_time = None
+
+    while True:
+        if if_not_blocked(last_wrong_attempt_time):
+            if login_execution():
+                break
+            else:
+                last_wrong_attempt_time = datetime.now()
+
+    print("Verification successful")
+
+
+if __name__ == "__main__":
+    main()
 
 
 
