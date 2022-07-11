cbb5ac441edd872ff109a2a616d8b4ec4ff0f08f
Mononkero <a.roshiari@gmail.com>
2022-07-11 20:49:38 +0300

    game 1.1

--- Game/Game.py
+++ Game/Game.py
@@ -7,30 +7,37 @@ wins = {"rock": ["scissors", "lizard"],
         "spock": ["rock", "scissors"]
 }
 
+
 def get_user_choise():
     print("Hello! You`re playing: Rock-paper-scissors-lizard-spock")
     while True:
-        user_input = input("Your choice (rock paper scissors lizard spock)?\n")
+        user_input = input("Choose your gesture (rock paper scissors lizard spock)?\n")
         if user_input in wins:
-            #print("Correct input")
+            print("Player`s choice:", user_input)
             return user_input
         else:
             print("Invalid input.")
 
 
 def play_again():
-    choice = input("Repeat (Y/N)?")
-    try:
-        return choice.lower() == 'y'
-    except (AttributeError, TypeError, IndexError):
-        return False
+    while True:
+        choice = input("Repeat (Y/N)?")
+        if choice == "y":
+            game()
+            continue
+        elif choice == "n":
+            break
+        else:
+            print("Invalid input")
+
+
 
 def game():
     computer_choice = random.choice(list(wins.keys()))
     user_choice = get_user_choise()
-    print("Computer choise:", computer_choice)
+    print("Computer`s choice:", computer_choice)
     if computer_choice in wins[user_choice]:
-        print("You win!")
+        print("Player win!")
     elif user_choice in wins[computer_choice]:
         print("Computer wins")
     else:
